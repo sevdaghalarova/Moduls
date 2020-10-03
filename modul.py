a83dee72ad16b1329cdd243af53062672a212c43
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-04 01:20:54 +0400

    modul kullanmak

---
+++ modul.py
@@ -0,0 +1,7 @@
+# moduller hazir olabilir veya kendimiz yaza biliriz
+# hazir modullerin programimiza eklenmesi
+import math # math modulunu kodumuza ekledik
+from math import log2 # math modulunden sadece log2 fonksiyonu eklendi
+from math import* # math modulundeki tum fonksiyonlari kullana biliriz
+print(math.factorial(5))
+print(log2(10))
